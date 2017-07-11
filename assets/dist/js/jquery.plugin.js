6bb188fc051fab0e22c70ca2e419d25c8e69e1d5
Danny G Smith <dgs@riskiii.com>
2017-07-11 09:52:30 -0500

    added jQuery helper functions

--- assets/dist/js/jquery.plugin.js
+++ assets/dist/js/jquery.plugin.js
@@ -1,15 +1,24 @@
 ;( function( $, window, document, undefined ){
    'use strict';
 
+   var $tabberTabs;      // array of cached tab labels
+   var $tabberContents;  // array of cached contents
+
 
    /**
     * Initializes all scripts via document ready function
     */
    var init = function() {
-
+      $tabberTabs     = $( '.tabber---tab' );
+      $tabberContents = $tabberTabs.next();
+      $tabberTabs.on( 'click',
+                      { contentType: 'tabber' },   // pass type
+                      clickHandler );              // named function
    };
 
-
+   var clickHandler = function( event ) {
+      console.log( event );
+   };
 
 
    $( document ).ready( function(){
